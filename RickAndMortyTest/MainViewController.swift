05b5d21c9fc1f0bc93ce189ff88aea2034b955ff
gusarovFG <gusarovfg@gmail.com>
2023-08-20 23:48:26 +0400

    feat: Добавлен CollectionView на главный экран установлены констрейнты для CollectionView

--- RickAndMortyTest/MainViewController.swift
+++ RickAndMortyTest/MainViewController.swift
@@ -8,13 +8,20 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    private var charactersCollectionView = UICollectionView(frame: .zero)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = #colorLiteral(red: 0.01568627451, green: 0.04705882353, blue: 0.1176470588, alpha: 1)
-        
-        
+
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        self.createConstraints()
+    }
+    
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -24,5 +31,40 @@ class MainViewController: UIViewController {
         
         self.title = "Characters"
     }
+    
+    private func setupViews() {
+        
+        self.view.backgroundColor = #colorLiteral(red: 0.01568627451, green: 0.04705882353, blue: 0.1176470588, alpha: 1)
+        
+        self.charactersCollectionView.dataSource = self
+        self.charactersCollectionView.delegate = self
+    }
+    
+    private func createConstraints() {
+        
+        self.view.addSubview(self.charactersCollectionView)
+        self.charactersCollectionView.frame = .zero
+        self.charactersCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            self.charactersCollectionView.topAnchor.constraint(equalTo: (self.navigationController?.navigationBar.bottomAnchor)!, constant: 31),
+            self.charactersCollectionView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            self.charactersCollectionView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            self.charactersCollectionView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: 20),
+        ])
+        
+        self.charactersCollectionView.backgroundColor = .blue
+    }
 }
 
+extension MainViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+}
