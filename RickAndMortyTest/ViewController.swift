85c2e504460e0c3608cb7e8b1f7b04a0fef7021a
gusarovFG <gusarovfg@gmail.com>
2023-08-20 22:10:04 +0400

    Initial Commit

---
+++ RickAndMortyTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyTest
+//
+//  Created by Фаддей Гусаров on 8/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
